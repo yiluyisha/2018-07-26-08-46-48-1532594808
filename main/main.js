1b123f810bf540c3e581bf34ef67c91988f0c0fa
xiaoming <m13750434238_1@163.com>
2018-07-30 16:27:13 +0800

    Finished main.js

--- main/main.js
+++ main/main.js
@@ -1,18 +1,54 @@
 module.exports = function main() {
     let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
     console.log(`o) 最小值 = ${sequence.minimum()}
-// Write your code here
-`);
+o) 最大值 = ${sequence.maximun()}
+o) 元素数量 = ${sequence.get_length()}
+o) 平均值 = ${sequence.average()}`);
 };
 
 class Sequence {
   constructor(input) {
-    // Write your code here
+    if (input instanceof Array) {
+      if (input.length == 0) {
+        console.log("The input is empty.");
+      } else {
+        this.input = input;
+      }
+    } else {
+      console.log("The input is not an array.");
+    }
   }
 
   minimum() {
-    // Write your code here
+    var min = this.input[0];
+    for (var i = 1; i < this.input.length; i++) {
+      if (this.input[i] < min) {
+        min = this.input[i];
+      }
+    }
+    return min;
   }
 
-  // Write your code here
+  maximun() {
+    var max = this.input[0];
+    for (var i = 1; i < this.input.length; i++) {
+      if (this.input[i] > max) {
+        max = this.input[i];
+      }
+    }
+    return max;
+  }
+
+  get_length() {
+    return this.input.length;
+  }
+
+  average() {
+    var sum = 0;
+    for (var i = 0; i < this.input.length; i++) {
+      sum += this.input[i];
+    }
+    return (sum / this.input.length).toFixed(2);
+  }
+
 }
